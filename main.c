cbe1b1f750f49755f2b99f4c97ba977b56b05df7
Husain Alshaikahmed <alshaikhahmed@hotmail.com>
2024-02-16 22:52:29 -0600

    added chat protocol and server

--- main.c
+++ main.c
@@ -8,31 +8,9 @@
 #define SERVER "127.0.0.1"
 #define BUFFER_SIZE 1024 // Buffer size for data transmission
 
-struct sockaddr_in* createIP4Address(char *ip, int port);
 
-SOCKET createTCPIp4Socket();
 
-int connectToServer(SOCKET socketFD, struct sockaddr_in address);
 
-struct sockaddr_in* createIP4Address(char *ip, int port){
-    // Define the server address and port
-//    char* ip = "142.250.188.46"; // IP address for the server
-    struct sockaddr_in  *address = malloc(sizeof(struct sockaddr_in)); // Struct for storing the server address
-    if(address == NULL){
-        perror("Malloc failed");
-        exit(EXIT_FAILURE);
-    }
-
-    address->sin_family = AF_INET; // Setting the address family to IPv4
-    address->sin_port = htons(PORT); // Converting the PORT number to network byte order
-
-   if(inet_pton(AF_INET, ip, &address->sin_addr)<=0){
-       printf("Invalid address/ Address not supported \n");
-       free(address);
-       exit(EXIT_FAILURE);
-   }
-    return address;
-}
 
 //////////////////////////////// MAIN /////////////////////////////////////////////////////////////////////
 int main() {
@@ -69,25 +47,3 @@ int main() {
     return result; // Use the result of the connection attempt
 }
 /////////////////////////////// END OF MAIN////////////////////////////////////////////////////////////
-int connectToServer(SOCKET socketFD, struct sockaddr_in address) {
-    int result = connect(socketFD, (struct sockaddr *)&address, sizeof(address));
-    if (result == SOCKET_ERROR) {
-        int errorCode = WSAGetLastError();
-        printf("Connect failed with error: %ld\n", errorCode);
-        if (errorCode == WSAETIMEDOUT) {
-            printf("Connection timed out.\n");
-        } else if (errorCode == WSAECONNREFUSED) {
-            printf("Connection refused.\n");
-        }
-        closesocket(socketFD);
-        WSACleanup();
-        return -1;
-    } else {
-        printf("Connection was successful\n");
-        return 0;
-    }
-}
-
-SOCKET createTCPIp4Socket(){
-   return socket(AF_INET, SOCK_STREAM, 0);
-};
---
+++ server.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <ws2tcpip.h> // For Windows socket definitions and functions
+#include <winsock2.h> // For TCP/IP protocols and internet functionality
+
+int main() {
+
+    return 0;
+}
